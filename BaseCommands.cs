e4babca445ac54835a2b4031d025c547f9dfdd94
Zontreck <tarapiccari@gmail.com>
2020-03-10 14:33:58 -0700

    New version for BlankBot

---
+++ BaseCommands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bot.CommandSystem;
+using OpenMetaverse;
+using Bot;
+using Newtonsoft.Json;
+
+
+namespace BlankBot
+{
+    public class BaseCommands
+    {
+        private struct NewProgram
+        {
+            public string type;
+            public string newProgram;
+
+            public NewProgram(string newProg)
+            {
+                type = "assignProgram";
+                newProgram = newProg;
+            }
+
+            public string GetStr()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+        [CommandGroupMaster("Base")]
+        [CommandGroup("assign",0,1, "assign [string:DLLName] - Sets DLLName as the active program", MessageHandler.Destinations.DEST_LOCAL | MessageHandler.Destinations.DEST_GROUP | MessageHandler.Destinations.DEST_AGENT)]
+        public void assignDLL(UUID client, int level, GridClient grid, string[] additionalArgs,
+                                MessageHandler.MessageHandleEvent MHE, MessageHandler.Destinations source,
+                                CommandRegistry registry, UUID agentKey, string agentName)
+        {
+            MHE(MessageHandler.Destinations.DEST_ACTION, UUID.Zero, new NewProgram(additionalArgs[0]).GetStr());
+        }
+    }
+}
--- Class1.cs
+++ Class1.cs
@@ -13,8 +13,6 @@ namespace BlankBot
 
         public MessageHandler.MessageHandleEvent MHE;
         public GridClient m;
-        CommandProcessor cp = null;
-        SysOut Log = SysOut.Instance;
 
         public string ProgramName
         {
@@ -27,15 +25,13 @@ namespace BlankBot
         {
             get
             {
-                return 1.0f;
+                return 1.1f;
             }
         }
 
         public string getTick()
         {
-            string movDat = cp.newTickStr;
-            cp.newTickStr = "";
-            return movDat;
+            return "";
         }
 
         public void LoadConfiguration()
@@ -47,48 +43,16 @@ namespace BlankBot
 
         public void onIMEvent(object sender, InstantMessageEventArgs e)
         {
-            try
-            {
-
-                FileStream FS = new FileStream("debugDump.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
-                StreamWriter sw = new StreamWriter(FS);
-                if (e.IM.Dialog == InstantMessageDialog.GroupInvitation)
-                {
-                    sw.WriteLine("IM Type [GroupInvite] from ID " + e.IM.IMSessionID.ToString() + " from avatar ID: " + e.IM.FromAgentID.ToString() + ", " + e.IM.FromAgentName);
-                }
-                else if (e.IM.Dialog == InstantMessageDialog.MessageFromObject)
-                {
-                    sw.WriteLine("IM Type [MessageFromObject] owned by avatar (" + e.IM.FromAgentID.ToString() + ", " + e.IM.FromAgentName + ") - Msg: " + e.IM.Message + " - SimName: " + e.Simulator.Name + " - ObjectName: " + e.IM.IMSessionID.ToString());
-                }
-
-
-                FileInfo fi = new FileInfo("debugDump.txt");
-                Console.WriteLine(" *** SAVED IM DATA TO '" + fi.FullName + "' *** ");
-
-                sw.Close();
-                FS.Close();
-            }
-            catch (Exception ex)
-            {
-                //
-            }
         }
 
         public void passArguments(string data)
         {
-            dynamic stuff = JsonConvert.DeserializeObject(data);
-            if (stuff.type == "chat")
-                cp.DoCommand(data, MHE);
 
         }
 
         public void run(GridClient client, MessageHandler MH, CommandRegistry registry)
         {
-            m = client;
-            Log = SysOut.Instance;
-            cp = new CommandProcessor(Log);
-            cp.newTickStr = "";
-            this.MHE = MH.callbacks;
+            BotSession.Instance.Logger.info(log: "BlankBot.dll is now active");
         }
 
 
